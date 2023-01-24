ffc0c2ce3ee4bceca257bb132ef8a01c456b0759
Bima Bagaskhoro <bimabagas644@gmail.com>
2023-01-24 10:59:19 +0700

    task 4 ui

---
+++ app/src/main/java/com/bimabagaskhoro/taskappphincon/ui/cart/CartFragment.kt
@@ -0,0 +1,19 @@
+package com.bimabagaskhoro.taskappphincon.ui.cart
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.bimabagaskhoro.taskappphincon.R
+
+class CartFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_cart, container, false)
+    }
+}
